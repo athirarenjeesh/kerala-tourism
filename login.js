ab96181864b1936c4db53ea0e1ac45980f8c9945
athira kp <athirakpkedharam@gmail.com>
2022-01-18 16:01:06 +0530

    change

---
+++ login.js
@@ -0,0 +1,9 @@
+var username=document.getElementById("usename").nodeValue;
+ var password=document.getElementById("password").Value;
+
+function validate() {
+ if (username==""||password==""){
+     alert("enter username and password to login ")
+ }
+
+}
